5ffa6440a384ad88d2640f1b5f1e2be42baed437
alcantarillamoralesrafa-cyber <alcantarillamoralesrafa@gmail.com>
2026-01-18 11:59:59 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+let moneda = 0
+input.onGesture(Gesture.ThreeG, function () {
+    moneda = randint(0, 1)
+    if (moneda == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
